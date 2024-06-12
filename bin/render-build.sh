a02b183d6857ce03897572b786ade05689185baa
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-06-12 23:59:10 +0800

    render-1

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,19 @@
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
+
+# bundle install
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
+
+
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
+
+chmod +x ./bin/rails
 
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails db:migrate
