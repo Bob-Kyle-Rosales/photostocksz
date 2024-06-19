9dfbba57abfac678d6ec6200101f07fbaba9b74d
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-06-19 22:32:57 +0800

    add apipie doc for photos controller

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,8 +1,42 @@
 class Api::V1::PhotosController < Api::V1::BaseController
-  include Pagy::Backend
   include PhotoConcern
   before_action :set_photo, only: %i[show update destroy]
 
+  resource_description do
+    short "Photos management"
+    description "API for managing photos"
+  end
+
+  api :GET, "api/v1/photos", "List all photos"
+  returns code: 200, desc: "Ok"
+  returns code: 400, desc: "Bad Request"
+  returns code: 401, desc: "Unauthorized"
+  returns code: 404, desc: "Not Found"
+  returns code: 500, desc: "Internal Server Error"
+  param :page, :number, desc: "Page number for pagination"
+  param :per_page, :number, desc: "Number of photos per page"
+  example <<-EOS
+    {
+      "photos": [
+        {
+          "id": 1,
+          "title": "MacBook",
+          "description": "Macbook from Apple",
+          "category": "Technology",
+          "likes": 83,
+          "taken_at": "2023-11-17T12:43:40.344Z",
+          "tags": "macbook, apple, laptop"
+        }
+      ],
+      "meta": {
+        "current_page": 1,
+        "next_page": null,
+        "prev_page": null,
+        "total_pages": 1,
+        "total_count": 1
+      }
+    }
+  EOS
   def index
     super
     pagy, @photos = pagy(Photo.all, items: params[:per_page] || 10)
@@ -13,13 +47,55 @@ class Api::V1::PhotosController < Api::V1::BaseController
     }
   end
 
+  api :GET, "/v1/photos/:id", "Show a photo"
+  param :id, :number, desc: "ID of the photo"
+  returns code: 200, desc: "Ok"
+  returns code: 400, desc: "Bad Request"
+  returns code: 401, desc: "Unauthorized"
+  returns code: 404, desc: "Not Found"
+  returns code: 500, desc: "Internal Server Error"
+  example <<-EOS
+    {
+      "id": 1,
+      "title": "MacBook",
+      "description": "Macbook from Apple",
+      "category": "Technology",
+      "likes": 83,
+      "taken_at": "2023-11-17T12:43:40.344Z",
+      "tags": "macbook, apple, laptop"
+    }
+  EOS
   def show
     super
     render json: @photo, serializer: PhotoSerializer
   end
 
+  api :POST, "/v1/photos", "Create a photo"
+  param :title, String, desc: "Title of the photo", required: true
+  param :description, String, desc: "Description of the photo", required: true
+  param :category, String, desc: "Category of the photo", required: true
+  param :tags, String, desc: "Tags for the photo", required: true
+  param :taken_at, DateTime, desc: "Date and time when the photo was taken", required: true
+  param :image, File, desc: "Image file", required: true
+  returns code: 200, desc: "Ok"
+  returns code: 400, desc: "Bad Request"
+  returns code: 401, desc: "Unauthorized"
+  returns code: 404, desc: "Not Found"
+  returns code: 500, desc: "Internal Server Error"
+  example <<-EOS
+    {
+      "id": 1,
+      "title": "MacBook",
+      "description": "Macbook from Apple",
+      "category": "Technology",
+      "likes": 83,
+      "taken_at": "2023-11-17T12:43:40.344Z",
+      "tags": "macbook, apple, laptop"
+    }
+  EOS
   def create
     super
+    @photo = Photo.new(photo_params)
     if @photo.save
       render json: @photo, serializer: PhotoSerializer, status: :created,
              location: api_v1_photo_url(@photo)
@@ -28,6 +104,30 @@ class Api::V1::PhotosController < Api::V1::BaseController
     end
   end
 
+  api :PUT, "/v1/photos/:id", "Update a photo"
+  param :id, :number, desc: "ID of the photo"
+  param :title, String, desc: "Title of the photo", required: true
+  param :description, String, desc: "Description of the photo", required: true
+  param :category, String, desc: "Category of the photo", required: true
+  param :tags, String, desc: "Tags for the photo", required: true
+  param :taken_at, DateTime, desc: "Date and time when the photo was taken", required: true
+  param :image, File, desc: "Image file", required: true
+  returns code: 200, desc: "Ok"
+  returns code: 400, desc: "Bad Request"
+  returns code: 401, desc: "Unauthorized"
+  returns code: 404, desc: "Not Found"
+  returns code: 500, desc: "Internal Server Error"
+  example <<-EOS
+    {
+      "id": 1,
+      "title": "MacBook",
+      "description": "Macbook from Apple",
+      "category": "Technology",
+      "likes": 83,
+      "taken_at": "2023-11-17T12:43:40.344Z",
+      "tags": "macbook, apple, laptop"
+    }
+  EOS
   def update
     super
     if @photo.update(photo_params)
@@ -38,8 +138,22 @@ class Api::V1::PhotosController < Api::V1::BaseController
     end
   end
 
+  api :DELETE, "/v1/photos/:id", "Delete a photo"
+  param :id, :number, desc: "ID of the photo"
+  returns code: 204, desc: "No Content"
+  returns code: 400, desc: "Bad Request"
+  returns code: 401, desc: "Unauthorized"
+  returns code: 403, desc: "Forbidden"
+  returns code: 404, desc: "Not Found"
+  returns code: 500, desc: "Internal Server Error"
+  example <<-EOS
+    {
+      "status": "success"
+    }
+  EOS
   def destroy
     super
+    @photo.destroy
     head :no_content
   end
 
@@ -53,5 +167,14 @@ class Api::V1::PhotosController < Api::V1::BaseController
       total_pages:  pagy.pages,
       total_count:  pagy.count
     }
+  end
+
+  def photo_params
+    params.require(:photo).permit(:title, :description, :category, :tags, :taken_at, :image,
+                                  :user_id)
+  end
+
+  def set_photo
+    @photo = Photo.find(params[:id])
   end
 end
