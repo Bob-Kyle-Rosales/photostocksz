7a4b348130056d359d179bcf83bd9a8f634cc70a
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-06-20 20:48:24 +0800

    Fix json return on api

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,3 +1,5 @@
 class PhotoSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :category, :likes, :taken_at, :tags
+
+  belongs_to :user, serializer: BaseUserSerializer
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,8 @@
-class UserSerializer < ActiveModel::Serializer
-  attributes :id, :firstname, :lastname, :email, :gender, :photos
+class BaseUserSerializer < ActiveModel::Serializer
+  attributes :id, :firstname, :lastname, :email, :gender
+end
+
+
+class UserSerializer < BaseUserSerializer
+  has_many :photos, serializer: PhotoSerializer
 end
