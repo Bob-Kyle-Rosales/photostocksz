812e19957ef6eec74b1a32cfbbb3eb43512d307a
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-06-23 18:45:06 +0800

    transfer files to api v1 folder

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,5 +1,4 @@
-class RegistrationsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create]
+class Api::V1::RegistrationsController < Api::V1::BaseController
   skip_before_action :authenticate_user, only: [:create]
 
   def create
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,12 +1,11 @@
-class SessionsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create]
+class Api::V1::SessionsController < Api::V1::BaseController
   skip_before_action :authenticate_user, only: [:create]
 
   def create
     @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
+    if @user&.authenticate(params[:password])
       token = JsonWebToken.encode({ email: @user.email })
-      render json: { auth_token: token }
+      render json: { 'auth-token': token }
     else
       render json: { error: "Incorrect Email Or password" }, status: :unauthorized
     end
